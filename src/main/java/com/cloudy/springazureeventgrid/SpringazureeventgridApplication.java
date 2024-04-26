256202fd33f2383b3eac70184407f8cae174a425
sarav <sarav@demo.com>
2024-04-26 14:41:03 +0100

    Base Spring Boot app with azure DM

---
+++ src/main/java/com/cloudy/springazureeventgrid/SpringazureeventgridApplication.java
@@ -0,0 +1,13 @@
+package com.cloudy.springazureeventgrid;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringazureeventgridApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringazureeventgridApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cloudy/springazureeventgrid/SpringazureeventgridApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cloudy.springazureeventgrid;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringazureeventgridApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
